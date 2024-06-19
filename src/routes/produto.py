b6d228dfdaef6435942e8db54f50ded234a7642f
ALUNO <sergio.marques@aluno.ifsp.edu.br>
2024-06-19 20:43:04 -0300

    Inclusao Remover produto e formatação de paginação

--- src/routes/produto.py
+++ src/routes/produto.py
@@ -2,6 +2,7 @@ from base64 import b64encode
 
 from flask import Blueprint, redirect, url_for, flash, render_template, request, Response, abort
 from flask_login import login_required
+from werkzeug.exceptions import NotFound
 
 from src.forms.produto import ProdutoForm
 from src.models.categoria import Categoria
@@ -14,7 +15,7 @@ bp = Blueprint('produto', __name__, url_prefix="/produto")
 @login_required
 def add():
     if Categoria.is_empty():
-        flash("Impossíveç adicionar produto. Adicine pelo menos uma categoria",
+        flash("Impossível adicionar produto. Adicione pelo menos uma categoria",
               category='warning')
         return redirect(url_for('categoria.add'))
 
@@ -91,17 +92,40 @@ def edit(produto_id):
                            title="Alterar um produto",
                            produto=produto)
 
+@bp.route('/delete;<uuid:produto_id>', methods=['GET'])
+@login_required
+def delete(produto_id):
+    produto = Produto.get_by_id(produto_id)
+    if produto is None:
+        flash("Produto inexistente", category='danger')
+        return redirect(url_for('produto.lista'))
+
+    db.session.delete(produto)
+    db.session.commit()
+    flash("Produto removido!", category='success')
+    return redirect(url_for('produto.lista'))
+
 
 
 @bp.route('/lista', methods=['GET', 'POST'])
 def lista():
-    sentenca = db.select(Produto).order_by(Produto.nome)
-    rset = db.session.execute(sentenca).scalars()
+    page = request.args.get('page', type=int, default=1)
+    pp = request.args.get('pp', type=int, default=25)
 
+    sentenca = db.select(Produto).order_by(Produto.nome)
+
+    try:
+        rset = db.paginate(sentenca, page=page, per_page=pp, error_out=True)
+    except NotFound:
+        flash(f"Não temos produtos na página {page}. Apresentando página 1")
+        page = 1
+        rset = db.paginate(sentenca, page=page, per_page=pp, error_out=False)
 
     return render_template('produto/lista.jinja2',
                             title="Lista de produtos",
-                            rset=rset)
+                            rset=rset,
+                           page=page,
+                           pp=pp)
 
 
 
